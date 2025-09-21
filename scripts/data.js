40a18b3b227c92ed0ae1510f23b42cd5e6d9c575
Bend Vladislav <vlad.bend@inbox.ru>
2025-09-21 13:23:15 +0300

    Frontend in university begin. Html

---
+++ scripts/data.js
@@ -0,0 +1,113 @@
+const gridData = {
+    head: "Lorem ipsum dolor sit amet.",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis error dolorem ipsa dolore facere est consequuntur aut, eos doloribus voluptate?",
+    imgs: [
+        "grid1.png",
+        "grid2.png",
+        "grid3.png",
+        "grid4.png",
+        "grid5.png",
+        "grid6.png",
+        "grid7.png",
+        "grid8.png",
+        "grid9.png"
+    ]
+}
+
+const tabsData = {
+    heads: [
+        "Find relevant media contacts - multiline title",
+        "Another amazing feature",
+        "And yet... another truly fascinating feature"
+    ],
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quidem ipsam ratione dicta quis cupiditate consequuntur laborum ducimus iusto velit.",
+    imgs: [
+        "tab1.png",
+        "tab2.png",
+        "tab3.png"
+    ]
+}
+
+const carouselData = {
+    imgs: [
+        "carousel1.png",
+        "carousel2.png",
+        "carousel3.png"
+    ],
+    authorsImgs: [
+        "author1.png",
+        "author2.png",
+        "author3.png"
+    ],
+    names: [
+        "Clyde Edwards",
+        "Jimmy Hunter",
+        "Marjorie Morgan"
+    ],
+    roles: [
+        "Very Serious Man",
+        "Sigma Male University Graduate",
+        "Chief Chad Officer"
+    ],
+    feedbacks: [
+        "\"Really good. I am so pleased with this product. I didn't even need training.\"",
+        "\"It's really wonderful. I use saas product often. Thank You! Saas product has really helped our business.\"",
+        "\"Since I invested in saas product I made over 100,000 dollars profits. It really saves me time and effort. saas product is exactly what our business has been lacking.\""
+    ]
+}
+
+const partnersImgs = [
+    "partner1.png",
+    "partner2.png",
+    "partner3.png",
+    "partner4.png",
+    "partner5.png",
+    "partner6.png"
+]
+
+const footerImgs = [
+    "footer1.png",
+    "footer2.png",
+    "footer3.png"
+]
+
+const footerInfo = [
+    {
+        head: "Company",
+        info: [
+            "Privacy Policy",
+            "Cookies Policy"
+        ]
+    },
+    
+    {
+        head: "Product",
+        info: [
+            "Features",
+            "Something",
+            "Something else",
+            "And something else"
+        ]
+    },
+
+    {
+        head: "Knowledge",
+        info: [
+            "Blog",
+            "Contact",
+            "FAQ",
+            "Help Center"
+        ]
+    },
+
+    {
+        head: "Something",
+        info: [
+            "Features2",
+            "Something2",
+            "Something else2",
+            "And something else2"
+        ]
+    }
+]
+
